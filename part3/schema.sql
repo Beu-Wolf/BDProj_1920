683a15e3d6552af45d6566ae7131437409b68801
Beu-Wolf <daniel.gseara@gmail.com>
2019-11-09 16:47:01 +0000

    All tables added, need to discuss RI

--- part3/schema.sql
+++ part3/schema.sql
@@ -6,6 +6,9 @@ drop table duplicado cascade;
 drop table utilizador cascade;
 drop table utilizador_qualificado cascade;
 drop table utilizador_regular cascade;
+drop table incidencia cascade;
+drop table proposta_de_correcao cascade;
+drop table correcao cascade;
 
 create table local_publico (
     latitude      numeric(8, 6) not null,
@@ -95,6 +98,41 @@ create table utilizador_regular (
 
 --TODO: how to implement RI-6?
 
+create table incidencia (
+    anomalia_id        integer not null,
+    item_id            integer not null,
+    email              varchar(255) not null,
+    constraint pk_incidencia primary key(anomalia_id),
+    constraint fk_incidencia_anomalia foreign key(anomalia_id)
+                references anomalia(id),
+    constraint fk_incidencia_item foreign key(item_id)
+                references item(id),
+    constraint fk_incidencia_utilizador foreign key(email)
+                references utilizador(email)
+);
 
+
+create table proposta_de_correcao (
+    email              varchar(255) not null unique,
+    nro                integer not null,
+    data_hora          timestamp not null,                  --Is it Timestamp?
+    texto              text not null,
+    constraint pk_proposta_de_correcao primary key(nro),   --Is this the primary key or email?
+    constraint fk_proposta_email foreign key(email)
+                 references utilizador_qualificado(email)
+);
+
+create table correcao (
+    email              varchar(255) not null,
+    nro                integer not null,
+    anomalia_id        integer not null,
+    constraint pk_correcao primary key(email, nro, anomalia_id),
+    constraint fk_correcao_proposta foreign key(nro)
+                references proposta_de_correcao(nro),
+    constraint fk_correcao_proposta2 foreign key(email) 
+                references proposta_de_correcao(email),
+    constraint fk_correcao_incidencia foreign key(anomalia_id)
+                references incidencia(anomalia_id)
+);
 
 
