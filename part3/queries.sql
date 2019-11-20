11f5bac211d1266417722c26b9d02f1e9fd02991
Marcelo Santos <marcelocmsantos@gmail.com>
2019-11-20 10:59:26 +0000

    Upcase sql keywords

--- part3/queries.sql
+++ part3/queries.sql
@@ -1,61 +1,61 @@
 -- Query 1
-with local_anomalia_count(latitude, longitude, nome, anomalia_count)
-as (select L.latitude, L.longitude, L.nome, COUNT(*)
-    from (local_publico natural join item) as L, incidencia as I
-    where L.id = I.item_id
-    group by (latitude, longitude))
-select latitude, longitude, nome
-from local_anomalia_count
-where anomalia_count = (select MAX(anomalia_count) from local_anomalia_count);
+WITH local_anomalia_count(latitude, longitude, nome, anomalia_count)
+AS (SELECT L.latitude, L.longitude, L.nome, COUNT(*)
+    FROM (local_publico NATURAL JOIN item) AS L, incidencia AS I
+    WHERE L.id = I.item_id
+    GROUP BY (latitude, longitude))
+SELECT latitude, longitude, nome
+FROM local_anomalia_count
+WHERE anomalia_count = (SELECT MAX(anomalia_count) FROM local_anomalia_count);
 
 
 -- Query 2
-with utilizador_regular_tmp(email, anomalia_count)
-     as (select I.email, COUNT(A.id)
-         from (utilizador_regular natural join incidencia) as I, anomalia A
-         where I.anomalia_id = A.id
-               and A.tem_anomalia_redacao = false
-               and TIMESTAMP '2019-01-01' <= A.ts
-               and A.ts < TIMESTAMP '2019-07-01'
-         group by I.email)
-select email
-from utilizador_regular_tmp
-where anomalia_count = (select MAX(anomalia_count) from utilizador_regular_tmp);
+WITH utilizador_regular_tmp(email, anomalia_count)
+     AS (SELECT I.email, COUNT(A.id)
+         FROM (utilizador_regular NATURAL JOIN incidencia) AS I, anomalia A
+         WHERE I.anomalia_id = A.id
+               AND A.tem_anomalia_redacao = false
+               AND TIMESTAMP '2019-01-01' <= A.ts
+               AND A.ts < TIMESTAMP '2019-07-01'
+         GROUP BY I.email)
+SELECT email
+FROM utilizador_regular_tmp
+WHERE anomalia_count = (SELECT MAX(anomalia_count) FROM utilizador_regular_tmp);
 
 
 -- Query 3
-with norte_rio_maior as (select * from local_publico
-                         where latitude > 39.336775),
+WITH norte_rio_maior AS (SELECT * FROM local_publico
+                         WHERE latitude > 39.336775),
      utilizador_local(email, latitude, longitude)
-       as (select I.email, LP.latitude, LP.longitude
-           from incidencia as I, anomalia A,
-                (item natural join local_publico) as LP
-           where I.item_id = LP.id
-                 and I.anomalia_id = A.id
-                 and extract(year from A.ts) = 2019)
-select distinct email
-from utilizador_local U1
-where not exists (select latitude, longitude from norte_rio_maior
-                  except
-                  select latitude, longitude from utilizador_local U2
-                  where U1.email = U2.email);
+       AS (SELECT I.email, LP.latitude, LP.longitude
+           FROM incidencia AS I, anomalia A,
+                (item NATURAL JOIN local_publico) AS LP
+           WHERE I.item_id = LP.id
+                 AND I.anomalia_id = A.id
+                 AND extract(year FROM A.ts) = 2019)
+SELECT distinct email
+FROM utilizador_local U1
+WHERE NOT EXISTS (SELECT latitude, longitude FROM norte_rio_maior
+                  EXCEPT
+                  SELECT latitude, longitude FROM utilizador_local U2
+                  WHERE U1.email = U2.email);
 
 
 -- Query 4
-with incidencia_qual_sul(email, anomalia_id)
-       as (select I.email, I.anomalia_id
-           from (utilizador_qualificado natural join incidencia) as I,
-                (item natural join local_publico) as LP, anomalia A
-           where I.item_id = LP.id
-                 and I.anomalia_id = A.id
-                 and LP.latitude < 39.336775
-                 and extract(year from A.ts) = extract(year from current_date))
-select distinct U1.email
-from incidencia_qual_sul U1
-where exists (select U2.anomalia_id
-              from incidencia_qual_sul U2
-              where U2.email = U1.email
-              except
-              select C.anomalia_id
-              from (proposta_de_correcao natural join correcao) as C
-              where C.email = U1.email);
+WITH incidencia_qual_sul(email, anomalia_id)
+       AS (SELECT I.email, I.anomalia_id
+           FROM (utilizador_qualificado NATURAL JOIN incidencia) AS I,
+                (item NATURAL JOIN local_publico) AS LP, anomalia A
+           WHERE I.item_id = LP.id
+                 AND I.anomalia_id = A.id
+                 AND LP.latitude < 39.336775
+                 AND extract(year FROM A.ts) = extract(year FROM current_date))
+SELECT distinct U1.email
+FROM incidencia_qual_sul U1
+WHERE EXISTS (SELECT U2.anomalia_id
+              FROM incidencia_qual_sul U2
+              WHERE U2.email = U1.email
+              EXCEPT
+              SELECT C.anomalia_id
+              FROM (proposta_de_correcao NATURAL JOIN correcao) AS C
+              WHERE C.email = U1.email);
