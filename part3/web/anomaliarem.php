1b10862e765a8e4f719ce09d9432c0b8b91928f3
Marcelo Santos <marcelocmsantos@gmail.com>
2019-11-22 13:51:05 +0000

    Remove "anomalia" interactively

---
+++ part3/web/anomaliarem.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Remover Anomalia</title>
+    </head>
+    <body>
+    <h1>Remover Anomalia</h1>
+    <?php
+        require 'db.php';
+        try {
+            $db = connect_db();
+
+            if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+
+                $sql = "DELETE FROM anomalia WHERE id = :id";
+                $result = $db->prepare($sql);
+                $result->execute([':id' => $_POST["id"]]);
+
+                echo("<p>Anomalia {$_POST["id"]} removida!</p>");
+            }
+
+            $sql = "SELECT id, ts FROM anomalia;";
+            $result = $db->prepare($sql);
+            $result->execute();
+
+            echo("<table>\n");
+            echo('<th scope="col">ID</th>');
+            echo('<th scope="col">Timestamp</th>');
+            echo('<th></th>');
+            foreach($result as $row) {
+                echo("<tr>\n");
+                echo("<td>{$row['id']}</td>\n");
+                echo("<td>{$row['ts']}</td>\n");
+                echo("<td>");
+                echo("<form action=\"\" method=\"POST\">");
+                echo("<input type=\"hidden\" name=\"id\" value=\"{$row['id']}\">");
+                echo("<input type=\"submit\" value=\"Remover\">");
+                echo('</form>');
+                echo("</td>");
+                echo("</tr>\n");
+            }
+            echo("</table>");
+        } catch (PDOException $e) { 
+            echo("<p>ERROR: {$e->getMessage()}</p>");
+        }
+    ?>
+    </body>
+</html>
