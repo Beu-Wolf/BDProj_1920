d5b53fa1e1af1fe2f3c6195136a2dd7ae7a0cec2
Beu-Wolf <daniel.gseara@gmail.com>
2019-11-23 18:45:12 +0000

    local insert and query in same php file

--- part3/web/localins.php
+++ part3/web/localins.php
@@ -7,22 +7,34 @@
         <?php
             require "db.php";
             try {
-                $db = connect_db();
 
-                $latitude = $_REQUEST['latitude'];
-                $longitude = $_REQUEST['longitude'];
+                if($_SERVER["REQUEST_METHOD"] == 'POST') {
 
-                $local = $_REQUEST['local'];
+                    $db = connect_db();
+                    $latitude = $_REQUEST['latitude'];
+                    $longitude = $_REQUEST['longitude'];
 
-                $db->beginTransaction();
+                    $local = $_REQUEST['local'];
 
-                $sql = "INSERT INTO local_publico (latitude, longitude, nome) VALUES (:latitude, :longitude, :nome);";
-                $result = $db->prepare($sql);
-                $result->execute([':latitude' => $latitude, ':longitude' => $longitude, ':nome' => $local]);
-                echo("<p>Successfully added location</p>");
+                    $db->beginTransaction();
 
-                $db->commit();
-                $db = null;
+                    $sql = "INSERT INTO local_publico (latitude, longitude, nome) VALUES (:latitude, :longitude, :nome);";
+                    $result = $db->prepare($sql);
+                    $result->execute([':latitude' => $latitude, ':longitude' => $longitude, ':nome' => $local]);
+                    echo("<p>Successfully added location</p>");
+
+                    $db->commit();
+                    $db = null;
+                }
+                
+                echo("<form action=\"\" method=\"post\">");
+                echo("<p>Latitude: <input type=\"number\" step=\"0.000001\" name=\"latitude\" required></p>");
+                echo("<p>Longitude: <input type=\"number\" step=\"0.000001\" name=\"longitude\" required></p>");
+                echo("<p>Nome do local: <input type=\"text\" name=\"local\" required></p>");
+                echo("<p><input type=\"submit\" value=\"Submit\"></p>");
+                echo('</form>');
+                
+                
 
             } catch (PDOException $e) {
                 $db->rollback();
