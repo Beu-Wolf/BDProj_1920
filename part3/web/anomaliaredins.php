9ed2823e9e68bf33dc89facae7d029c311e0fb28
Marcelo Santos <marcelocmsantos@gmail.com>
2019-11-24 19:18:54 +0000

    Remove manual id insertion in anomalia
    IDs are serial.
    In the "anomalia_traducao" case, we need to know the id assigned
    to it in the "anomalia" table to match it up.
    To avoid querying "anomalia" with the biggest ID, we use RETURNING
    which gives us elements of the newly inserted row.

--- part3/web/anomaliaredins.php
+++ part3/web/anomaliaredins.php
@@ -10,7 +10,6 @@
 
                 $db = connect_db();
                 
-                $id = $_POST['id'];
                 $zona = $_POST['zona'];
                 $imagem = $_POST['imagem'];
                 $lingua = $_POST['lingua'];
@@ -20,10 +19,10 @@
                 try {
                     $db->beginTransaction();
                 
-                    $sql = "INSERT INTO anomalia(id, zona, imagem, lingua, ts, descricao, tem_anomalia_redacao) VALUES (:id, :zona, :imagem, :lingua, :ts, :descricao, False);";
+                    $sql = "INSERT INTO anomalia(zona, imagem, lingua, ts, descricao, tem_anomalia_redacao) VALUES (:zona, :imagem, :lingua, :ts, :descricao, False);";
                     
                     $result = $db->prepare($sql);
-                    $ret = $result->execute([':id' => $id, ':zona' => $zona, ':imagem' => $imagem, ':lingua' => $lingua, ':ts' => $ts, ':descricao' => $descricao]);
+                    $ret = $result->execute([':zona' => $zona, ':imagem' => $imagem, ':lingua' => $lingua, ':ts' => $ts, ':descricao' => $descricao]);
                     
                     if($ret) {
                         echo("<p>Successfully added anomalia</p>");
@@ -34,12 +33,7 @@
                     }
 
                 } catch(PDOException $e) {
-                    $msg = $e->getMessage();
-                    
-                    if(strstr($msg, "already exists")) {
-                        echo("<p>Anomalia {$id} already exists</p>");
-                    }
-                    
+                    echo("<p>ERROR; {$e->getMessage()}</p>");
                     $db->rollback();
                 }
                 
@@ -52,7 +46,6 @@
     
     ?>
     <form action="" method="post">
-        <p>id: <input type="number" name="id" required></p>
         <p>Zona1: <input type="text" name="zona" required></p>
         <p>Imagem: <input type="text" name="imagem" required></p>
         <p>Lingua: <input type="text" name="lingua" required></p>
--- part3/web/anomaliatradins.php
+++ part3/web/anomaliatradins.php
@@ -10,7 +10,6 @@
 
                 $db = connect_db();
                 
-                $id = $_POST['id'];
                 $zona = $_POST['zona'];
                 $imagem = $_POST['imagem'];
                 $lingua = $_POST['lingua'];
@@ -22,10 +21,11 @@
                 try {
                     $db->beginTransaction();
                 
-                    $sql = "INSERT INTO anomalia(id, zona, imagem, lingua, ts, descricao, tem_anomalia_redacao) VALUES (:id, :zona, :imagem, :lingua, :ts, :descricao, True);";
+                    $sql = "INSERT INTO anomalia(zona, imagem, lingua, ts, descricao, tem_anomalia_redacao) VALUES (:zona, :imagem, :lingua, :ts, :descricao, True) RETURNING id;";
                     
                     $result = $db->prepare($sql);
-                    $ret = $result->execute([':id' => $id, ':zona' => $zona, ':imagem' => $imagem, ':lingua' => $lingua, ':ts' => $ts, ':descricao' => $descricao]);
+                    $ret = $result->execute([':zona' => $zona, ':imagem' => $imagem, ':lingua' => $lingua, ':ts' => $ts, ':descricao' => $descricao]);
+                    $id = $result->fetchAll()[0]['id'];
                     
                     $sql = "INSERT INTO anomalia_traducao(id, zona2, lingua2) VALUES(:id, :zona2, :lingua2);";
                     
@@ -38,22 +38,15 @@
                         $db->commit();
                     } else {
                         echo("<p>Error inserting anomaly</p>");
-                        $db->rollbak();
+                        $db->rollback();
                     }
 
                 } catch(PDOException $e) {
-                    $msg = $e->getMessage();
-                    
-                    if(strstr($msg, "already exists")) {
-                        echo("<p>Anomalia {$id} already exists</p>");
-                    }
-                    
+                    echo("<p>ERROR; {$e->getMessage()}</p>");
                     $db->rollback();
                 }
                 
-                
                 $db = null;
-            
             }
 
         } catch (PDOException $e) {
@@ -63,7 +56,6 @@
 
 
     <form action="" method="post">
-        <p>id: <input type="number" name="id" required></p>
         <p>Zona1: <input type="text" name="zona" required></p>
         <p>Imagem: <input type="text" name="imagem" required></p>
         <p>Lingua: <input type="text" name="lingua" required></p>
